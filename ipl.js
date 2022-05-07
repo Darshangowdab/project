de800a3ed2fb7b6580ef3eb2e23c898ffbd02565
Darshangowdab <darshanb26498@gmail.com>
2022-05-07 12:47:03 +0530

    darshan project

---
+++ ipl.js
@@ -0,0 +1,9 @@
+document.querySelector("#ipl").addEventListener("click",myiplFun);
+function myiplFun(){
+    window.location.href="https://www.bing.com/search?q=royal+challengers+bangalore&form=ANNTH1&refig=df5a91f1a77d4759aba2ede8d2be809e&sp=1&qs=LS&pq=royal+cha&sk=PRES1&sc=8-9&cvid=df5a91f1a77d4759aba2ede8d2be809e"
+  
+}
+document.querySelector("#hindi").addEventListener("click",myhindi);
+function myhindi(){
+    window.location.href="https://www.dnaindia.com/hindi"
+}
